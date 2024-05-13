01c59d4f5ef7cd7050684b504d9570e9a76a7591
Ayush Baliyan <helloayushbaliyan@gmail.com>
2024-05-13 22:42:55 +0530

    blog-website-using react js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>mega blog using app write</h1>
+    </>
+  )
+}
+
+export default App
